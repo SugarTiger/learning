be8addfebf8d56721dc9ec3d7528eb29b440a779
SugarTiger <1029109173@qq.com>
2017-08-30 18:13:09 +0800

    api

---
+++ webpack-vue-vueRouter/src/components/news.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    data() {
+        return {
+            apiUrl: "http://route.showapi.com/109-35",
+            showapi_sign: "11eb291d9eae49e191ca546967daf8c7",
+            showapi_appid: "45144",
+        }
+    }
+}
+</script>
